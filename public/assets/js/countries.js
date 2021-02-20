188db6c854963bbda7013fc30d076c0c16406ade
gabel35 <gabrieljose3135@gmail.com>
2021-02-20 18:50:02 -0500

    worked on home and blog handlebars

---
+++ public/assets/js/countries.js
@@ -0,0 +1,6 @@
+//grabbing api info for travel restrictions and appending it//
+$("#countryName").on("click", function(event) {
+    event.preventDefault();
+    var countryName = $("#countryName").val();
+    console.log(countryName);
+})
