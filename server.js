7396ac4ddbd1da30eaff3db96eba4a8dfdda994f
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-20 15:54:39 -0500

    Fixed routing issues with handlebars

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-// Requiring necessary npm packages
-var express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
 // var session = require("express-session");
-// Requiring passport as we've configured it
 // var passport = require("./config/passport");
+// const path = require("path");
 require("dotenv").config();
-
+// Requiring our models for syncing
 const db = require("./models");
 
 // Sets up the Express App
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// Requiring our models for syncing
-// const db = require("./models");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
-app.use(express.static("public"));
-
-// Routes
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
+app.use(express.static("views"));
 
 // Syncing our sequelize models and then starting our Express app
+
+app.get("/", (req, res) => {
+  res.render("Home");
+});
 
 db.sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
