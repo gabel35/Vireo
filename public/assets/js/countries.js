cd403334ab32f5d0f389d9610156f8ab28e1ea83
gabel35 <gabrieljose3135@gmail.com>
2021-02-20 20:16:56 -0500

    fixed connections with css and js files

--- public/assets/js/countries.js
+++ public/assets/js/countries.js
@@ -1,6 +1,24 @@
 //grabbing api info for travel restrictions and appending it//
-$("#countryName").on("click", function(event) {
+$("#countrySpa").on("click", function(event) {
     event.preventDefault();
-    var countryName = $("#countryName").val();
+    var countryName = $("#countrySpa").text();
+    console.log(countryName);
+})
+
+$("#countryDR").on("click", function(event) {
+    event.preventDefault();
+    var countryName = $("#countryDR").text();
+    console.log(countryName);
+})
+
+$("#countryRus").on("click", function(event) {
+    event.preventDefault();
+    var countryName = $("#countryRus").text();
+    console.log(countryName);
+})
+
+$("#countryMex").on("click", function(event) {
+    event.preventDefault();
+    var countryName = $("#countryMex").text();
     console.log(countryName);
 })
