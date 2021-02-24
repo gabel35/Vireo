9943964d643c0d3de59f8115f86c5ff28b9de025
gabel35 <gabrieljose3135@gmail.com>
2021-02-23 21:04:53 -0500

    carousel

--- public/assets/js/countries.js
+++ public/assets/js/countries.js
@@ -3,11 +3,39 @@ $("#countrySpa").on("click", function(event) {
     event.preventDefault();
     var countryName = $("#countrySpa").text();
     console.log(countryName);
+    // var bgImgs = [1,2,3,4,5,6,7];
+    var carouselDiv = $(".carousel-inner")
+    // for (let i = 0; i<bgImgs.length; i++){
+        var carouselImgs = `
+        <div class="carousel-item active">
+           <img src="assets/img/Spain/spain1.jpg" class="d-block w-100" alt="First slide">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/img/Spain/spain2.jpg" class="d-block w-100" alt="2 slide">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/img/Spain/spain3.jpg" class="d-block w-100" alt="3 slide">
+        </div>
+        <div class="carousel-item">
+            <img src="assets/img/Spain/spain4.jpg" class="d-block w-100" alt="4 slide">
+        </div>`;
+        console.log(carouselImgs)
+        carouselDiv.append(carouselImgs);
+        
+    // }
 })
 
-$("#countryDR").on("click", function(event) {
+
+
+$("#countryFra").on("click", function(event) {
     event.preventDefault();
-    var countryName = $("#countryDR").text();
+    var countryName = $("#countryFra").text();
+    console.log(countryName);
+})
+
+$("#countryUS").on("click", function(event) {
+    event.preventDefault();
+    var countryName = $("#countryUS").text();
     console.log(countryName);
 })
 
@@ -20,5 +48,11 @@ $("#countryRus").on("click", function(event) {
 $("#countryMex").on("click", function(event) {
     event.preventDefault();
     var countryName = $("#countryMex").text();
+    console.log(countryName);
+})
+
+$("#countryDR").on("click", function(event) {
+    event.preventDefault();
+    var countryName = $("#countryDR").text();
     console.log(countryName);
 })
