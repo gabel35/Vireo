18e480db2d7d249525f6d27090e4c98abb81ac7e
gabel35 <gabrieljose3135@gmail.com>
2021-02-25 02:15:12 -0500

    attempt at restrictions api

--- public/assets/js/countries.js
+++ public/assets/js/countries.js
@@ -1,13 +1,63 @@
-//grabbing api info for travel restrictions and appending it//
+// require("dotenv").config();
+
+//creating on click functions for the countries to apend info on home page//
 $("#countrySpa").on("click", function(event) {
     event.preventDefault();
+
+    var queryURL ="https://api.travelperk.com/travelsafe/restrictions/?origin=" + "US" + "&destination=" + "ES" + "&origin_type=country_code&destination_type=country_code&date=" + "2021-02-16";
+    console.log(queryURL);
+
+    const options = {
+        url: queryURL,
+        method: 'GET',
+        headers: {
+            'Api-Version': '1',
+            Authorization: 'ApiKey 5XSu8m.2H0SM6jrruHvlfn4SxjXHJwKAY1JV9Em',
+            'Accept-Language': 'en'
+          }
+      };
+      
+      fetch(options)
+        .then(response => console.log(response))
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET",
+    //     headers: {
+    //         "Api-Version": "1",
+    //         Authorization: "ApiKey " + "5XSu8m.2H0SM6jrruHvlfn4SxjXHJwKAY1JV9Em",
+    //         "Accept-Language": "en"
+    //       }
+    // }).then(function(response){
+    //     console.log(response)
+    //         var curDiv = $("#currencyDiv");
+    //         $("#currencyDiv").attr("class", "card");            
+    //         var countryCurrency = {
+    //             code: response[0].currencies[0].code,
+    //             name: response[0].currencies[0].name,
+    //             symbol: response[0].currencies[0].symbol
+    //         };
+    //         var appendCur = `
+    //             <div class="card-body">
+    //                 <h1 class="card-title">Currency</h1>
+    //                     <div class="card-body">
+    //                         <p> Code: ${countryCurrency.code}</p>
+    //                         <p> Name: ${countryCurrency.name}</p>
+    //                         <p> Symbol: ${countryCurrency.symbol}</p>
+    //                     </div>
+    //             </div>
+    //         `;
+    //     curDiv.append(appendCur);
+    // });
+
+
+    //grabbing API info for currency of the selected country and appending to apge//
     var countryName = $("#countrySpa").text();
     console.log(countryName);
     var queryURL = "https://restcountries.eu/rest/v2/name/" + countryName;
     console.log(queryURL);
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
     }).then(function(response){
         console.log(response)
             var curDiv = $("#currencyDiv");
@@ -29,6 +79,8 @@ $("#countrySpa").on("click", function(event) {
             `;
         curDiv.append(appendCur);
     });
+
+    // appending the buttons to book a hotel and flight
     var btnsDiv = $("#bookBtns");
     var appendBtns =`
     <a href="https://google.com/hotels" target="_blank">
