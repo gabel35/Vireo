a2804adff078ab7f35cdee7238c04abcac124b2d
Jonathan Gomez <jongomezdev@gmail.com>
2021-02-25 20:44:02 -0500

    Rendering functionality completed

--- routes/seed-helper-routes.js
+++ routes/seed-helper-routes.js
@@ -9,21 +9,21 @@ module.exports = (app) => {
     db.Country.create({ name: "Mexico" });
     db.Country.create({ name: "Dominican Republic" }).then((res) => {
       const top5s = [
-        { location_name: "Moma", category: "museums", CountryId: 1 },
-        { location_name: "Natural History", category: "museums", CountryId: 1 },
-        { location_name: "Whitney", category: "museums", CountryId: 1 },
-        { location_name: "All", category: "museums", CountryId: 1 },
-        { location_name: "None", category: "museums", CountryId: 1 },
-        { location_name: "Linguini", category: "restaurants", CountryId: 1 },
-        { location_name: "Pizza", category: "restaurants", CountryId: 1 },
-        { location_name: "Bread", category: "restaurants", CountryId: 1 },
-        { location_name: "Olives", category: "restaurants", CountryId: 1 },
-        { location_name: "Ceviche", category: "restaurants", CountryId: 1 },
-        { location_name: "overlook", category: "sights", CountryId: 1 },
-        { location_name: "mountain top", category: "sights", CountryId: 1 },
-        { location_name: "sea view", category: "sights", CountryId: 1 },
-        { location_name: "forest", category: "sights", CountryId: 1 },
-        { location_name: "church", category: "sights", CountryId: 1 },
+        { name: "Moma", category: "museums", CountryId: 1 },
+        { name: "Natural History", category: "museums", CountryId: 1 },
+        { name: "Whitney", category: "museums", CountryId: 1 },
+        { name: "All", category: "museums", CountryId: 1 },
+        { name: "None", category: "museums", CountryId: 1 },
+        { name: "Linguini", category: "restaurants", CountryId: 1 },
+        { name: "Pizza", category: "restaurants", CountryId: 1 },
+        { name: "Bread", category: "restaurants", CountryId: 1 },
+        { name: "Olives", category: "restaurants", CountryId: 1 },
+        { name: "Ceviche", category: "restaurants", CountryId: 1 },
+        { name: "overlook", category: "sights", CountryId: 1 },
+        { name: "mountain top", category: "sights", CountryId: 1 },
+        { name: "sea view", category: "sights", CountryId: 1 },
+        { name: "forest", category: "sights", CountryId: 1 },
+        { name: "church", category: "sights", CountryId: 1 },
       ];
       top5s.forEach((topfives) => {
         db.TopFive.create(topfives);
