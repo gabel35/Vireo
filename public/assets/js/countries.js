e975947eb25c12a339f31e8c9a9b8b89543c84cc
gabel35 <gabrieljose3135@gmail.com>
2021-02-27 09:44:38 -0500

    tied to page

--- public/assets/js/countries.js
+++ public/assets/js/countries.js
@@ -5,34 +5,39 @@ const countries = document.querySelectorAll(".countries").forEach((button) => {
     });
 });
 
-module.exports = (app) => {
-  app.get("/country/:country", (req, res) => {
+$(document).ready(() => {
+  let pathname = window.location.pathname;
+  console.log(pathname);
+  var pathArray = pathname.split('/');
+  console.log(pathArray);
+  if (pathArray[2]) {
+    loadCurrency(pathArray[2])
+  }
+});
 
-// $(".countries").on("click", function(event) {
-//     console.log()
-//     var countryName = $(event.target.text());
-var queryURL = "https://restcountries.eu/rest/v2/name/" + req.params.country;
-console.log(queryURL);
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function(response){
-    console.log(response)
-        var curDiv = $("#currencyDiv");
-  
-        var countryCurrency = {
-            code: response[0].currencies[0].code,
-            name: response[0].currencies[0].name,
-            symbol: response[0].currencies[0].symbol
-        };
-        console.log(response);
-        var appendCur = `
-                        <p> Code: ${countryCurrency.code}</p>
-                        <p> Name: ${countryCurrency.name}</p>
-                        <p> Symbol: ${countryCurrency.symbol}</p>
-        `;
-    curDiv.append(appendCur);
-});
-      
-});
-};
+const loadCurrency = countryName => {
+    // var countryName = ;
+  console.log(countryName)
+  var queryURL = "https://restcountries.eu/rest/v2/name/" + countryName;
+  console.log(queryURL);
+  $.ajax({
+      url: queryURL,
+      method: "GET",
+  }).then(function(response){
+      console.log(response)
+          var curDiv = $("#currencyDiv");
+    
+          var countryCurrency = {
+              code: response[0].currencies[0].code,
+              name: response[0].currencies[0].name,
+              symbol: response[0].currencies[0].symbol
+          };
+          console.log(response);
+          var appendCur = `
+                          <p> Code: ${countryCurrency.code}</p>
+                          <p> Name: ${countryCurrency.name}</p>
+                          <p> Symbol: ${countryCurrency.symbol}</p>
+          `;
+      curDiv.append(appendCur);
+  });
+}
