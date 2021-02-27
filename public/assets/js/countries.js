f05f9d7045abc4a1d394c9e0509f143c6a465d45
gabel35 <gabrieljose3135@gmail.com>
2021-02-27 09:56:56 -0500

    appending to page

--- public/assets/js/countries.js
+++ public/assets/js/countries.js
@@ -7,24 +7,19 @@ const countries = document.querySelectorAll(".countries").forEach((button) => {
 
 $(document).ready(() => {
   let pathname = window.location.pathname;
-  console.log(pathname);
   var pathArray = pathname.split('/');
-  console.log(pathArray);
   if (pathArray[2]) {
-    loadCurrency(pathArray[2])
+    loadCurrency(pathArray[2]);
+    loadContent(pathArray[2]);
   }
 });
 
 const loadCurrency = countryName => {
-    // var countryName = ;
-  console.log(countryName)
   var queryURL = "https://restcountries.eu/rest/v2/name/" + countryName;
-  console.log(queryURL);
   $.ajax({
       url: queryURL,
       method: "GET",
   }).then(function(response){
-      console.log(response)
           var curDiv = $("#currencyDiv");
     
           var countryCurrency = {
@@ -32,12 +27,35 @@ const loadCurrency = countryName => {
               name: response[0].currencies[0].name,
               symbol: response[0].currencies[0].symbol
           };
-          console.log(response);
           var appendCur = `
-                          <p> Code: ${countryCurrency.code}</p>
-                          <p> Name: ${countryCurrency.name}</p>
-                          <p> Symbol: ${countryCurrency.symbol}</p>
+          <div class="card" style="margin: 10px;">
+            <div class="card-body">
+                <h1 class="card-title">Currency</h1>
+                <div class="card-body">
+                  <p> Code: ${countryCurrency.code}</p>
+                  <p> Name: ${countryCurrency.name}</p>
+                  <p> Symbol: ${countryCurrency.symbol}</p>
+                </div>
+            </div>
+          </div>
           `;
       curDiv.append(appendCur);
   });
+}
+
+const loadContent = () => {
+  var kpDiv = $("#keyPhrases");
+  var appendPhrases =`
+    <div class="card">
+      <div class="card-body">
+          <h1 class="card-title">Key Phrases: </h1>
+          <div class="card-body">
+              {{#each language}}
+                  <p>{{dataValues.name}}</p>
+              {{/each}}
+          </div>
+      </div>
+    </div>
+    `;
+  kpDiv.append(appendPhrases)
 }
