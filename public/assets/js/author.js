b6a889a76061254d90095ec369f498974191a46f
yourdilemma <k.iankovskaia@gmail.com>
2021-02-28 18:04:02 -0500

    moved link

--- public/assets/js/author.js
+++ public/assets/js/author.js
@@ -138,18 +138,20 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml3');
+var textWrapper = document.querySelector('.ml2');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml3 .letter',
+    targets: '.ml2 .letter',
+    scale: [4,1],
     opacity: [0,1],
-    easing: "easeInOutQuad",
-    duration: 2250,
-    delay: (el, i) => 150 * (i+1)
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
   }).add({
-    targets: '.ml3',
+    targets: '.ml2',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
