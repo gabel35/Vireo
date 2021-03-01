d8884370c757489623873fdb038bf589f7aafd16
yourdilemma <k.iankovskaia@gmail.com>
2021-02-28 19:05:27 -0500

    finally animation works

--- public/assets/js/author.js
+++ public/assets/js/author.js
@@ -140,20 +140,18 @@ document.addEventListener("DOMContentLoaded", () => {
 // animated text
 
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml2');
+var textWrapper = document.querySelector('.ml3');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml2 .letter',
-    scale: [4,1],
+    targets: '.ml3 .letter',
     opacity: [0,1],
-    translateZ: 0,
-    easing: "easeOutExpo",
-    duration: 950,
-    delay: (el, i) => 70*i
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
   }).add({
-    targets: '.ml2',
+    targets: '.ml3',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
